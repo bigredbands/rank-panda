9a4e6e06120272362f10f8e34a0046092019d795
Victoria Dye <3619353+vdye@users.noreply.github.com>
2023-06-02 00:01:14 -0700

    *: more tabs to spaces
    For some reason, 0a8c9ec (Convert tabs to spaces, 2022-08-29) did not
    completely replace tabs with spaces. Finish the replacement here.

--- src/org/bigredbands/mb/EntryPoint.java
+++ src/org/bigredbands/mb/EntryPoint.java
@@ -3,17 +3,17 @@ package org.bigredbands.mb;
 import org.bigredbands.mb.controllers.MainController;
 
 /**
- * 
+ *
  * This class handles launching the program and creates the controller
  *
  */
 public class EntryPoint {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		new MainController();
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new MainController();
+    }
 
 }
