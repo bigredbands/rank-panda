87155a518a939e2fccb3f2a71143a035215fa8db
Victoria Dye <3619353+vdye@users.noreply.github.com>
2023-08-01 17:13:45 -0700

    PDFGenerator.java: improve drill instruction formatting
    Revamp the formatting of drill instructions:
    
    - rank names are bolded to distinguish from move instructions
    - line wrapping happens after full command tokens rather than words
    - if drill overflows one page, a new page is created for the overflow
    
    Create the new 'ConsolidatedDrillInstruction' model to represent a
    collection of ranks with the same drill commands for a given move.
    
    Signed-off-by: Victoria Dye <3619353+vdye@users.noreply.github.com>

--- src/main/java/org/bigredbands/mb/utils/PDFStringUtils.java
+++ src/main/java/org/bigredbands/mb/utils/PDFStringUtils.java
@@ -11,4 +11,49 @@ public class PDFStringUtils {
     {
         return font.getStringWidth(str) / 1000.0f * fontSize;
     }
+
+    public static String[] wordWrap(String stringToWrap,
+        float widthLimit, float initialOffset,
+        String wrapOn,
+        PDFont font, float fontSize) throws IOException
+    {
+        ArrayList<String> strs = new ArrayList<String>();
+        String[] tokens = stringToWrap.split(wrapOn);
+
+        float remainingLine = widthLimit - initialOffset;
+        String line = "";
+        for (int i = 0; i < tokens.length; i++) {
+            String token = tokens[i];
+            if (i < tokens.length - 1) token += wrapOn;
+
+            float tokenSize = stringWidth(token, font, fontSize);
+
+            // The token fits on the current line
+            if (remainingLine >= tokenSize) {
+                line += token;
+                remainingLine -= tokenSize;
+                continue;
+            }
+
+            // Token doesn't fit on the line
+            if (line.isEmpty()) {
+                // Just put the token on its own line (even though it's too long)
+                strs.add(token.trim());
+                continue;
+            } else {
+                // Token overflows this line - put it on the next one
+                strs.add(line.trim());
+
+                // Reset the next line
+                remainingLine = widthLimit - tokenSize;
+                line = token;
+            }
+        }
+
+        if (!line.isEmpty()) {
+            strs.add(line.trim());
+        }
+
+        return strs.toArray(new String[strs.size()]);
+    }
 }
